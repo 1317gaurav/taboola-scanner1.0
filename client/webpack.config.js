e3ce45d2764d21521c1c5961f746d1016f9676ed
Arun.n <arun.n@taboola.com>
2020-09-22 09:10:04 +0530

    Final Docker live

---
+++ client/webpack.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  
+    devServer: {
+        disableHostCheck: true,
+        allowedHosts: [
+            'ti-dev001.taboolasyndication.com',
+            'taboolasyndication.com'
+          ]
+    }
+ }
